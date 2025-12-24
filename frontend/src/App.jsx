b760a17b6d2782964cb7f8ded37f7a49bfccb929
Amar Patra <amarpatra932@gmail.com>
2025-12-24 20:16:40 +0530

    Frontend Setup Successfully

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App() {
+  return (
+    <div className="h-full w-full">
+      <AppRoutes />
+    </div>
+  );
+}
---
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="h-full flex items-center justify-center text-xl">
+      404 — Page Not Found
+    </div>
+  );
+}
